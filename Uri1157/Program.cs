7334386dc2aca16e1fb36bf3aff63513e274b409
Rubem <40901612+Rubem1287@users.noreply.github.com>
2021-02-16 16:38:27 -0300

    Add files via upload

---
+++ Uri1157/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uri1157
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+
+            for (int c = 1; c <= n; c++)
+            {
+              
+                if (n % c == 0)
+                {
+                    Console.WriteLine(c);
+                }
+            }
+        }
+    }
+}
